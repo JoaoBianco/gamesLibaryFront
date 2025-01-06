693ab2917b44077ec9ed90346f763afda8cf385c
JoaoBianco <joaops.bianco@gmail.com>
2025-01-05 21:00:08 -0300

    fix: remove duplicate games

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,12 +1,13 @@
 "use client"
 
 import { RawgGamesList } from "@/app/shared/models/game.model"
+import { uniqBy } from "lodash"
 import { useEffect, useState } from "react"
 import Spinner from "../(components)/spinner/Spinner"
 import HomeWrapper from "./(components)/HomeWrapper"
 
 export default function Home() {
-  const [games, setGames] = useState<RawgGamesList>()
+  const [allGames, setAllGames] = useState<RawgGamesList>()
   const [page, setPage] = useState(1)
   const [loading, setLoading] = useState(false)
   const [error, setError] = useState(false)
@@ -16,17 +17,23 @@ export default function Home() {
   }, [page])
 
   async function fetchGames(page: number) {
-    if (loading) return
+    if (allGames?.next?.includes(`page=${page + 1}`) || loading) {
+      return
+    }
     setLoading(true)
     try {
       await fetch(`http://localhost:3000/api/rawgGames?page=${page}`).then(
         (res) => {
           res.json().then((games: RawgGamesList) => {
-            setGames((prev) =>
+            const cleanGames = uniqBy(
+              [...(allGames?.results || []), ...games.results],
+              "id"
+            )
+            setAllGames((prev) =>
               prev
                 ? {
                     ...prev,
-                    results: [...prev.results, ...games.results],
+                    results: cleanGames,
                   }
                 : games
             )
@@ -46,8 +53,8 @@ export default function Home() {
 
   return (
     <>
-      {games?.results && (
-        <HomeWrapper games={games.results} setPage={setPage} />
+      {allGames?.results && (
+        <HomeWrapper games={allGames.results} setPage={setPage} />
       )}
       {loading && <Spinner />}
     </>
